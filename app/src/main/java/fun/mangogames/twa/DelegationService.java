ddf6c3a36a6566068986af59f873fa77b4a22bdb
rajeshmn47 <rajeshmn47@gmail.com>
2025-05-24 20:14:10 +0530

    new code

---
+++ app/src/main/java/fun/mangogames/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package fun.mangogames.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
